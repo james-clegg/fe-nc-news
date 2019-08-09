452672e4597b1066d3aae328043d236fb645e004
James <j.clegg1@hotmail.co.uk>
2019-08-09 09:38:29 +0100

    _redirects

---
+++ build/precache-manifest.f926eccab4a51699a8077245d6bb41ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fddd8287b2f94fe750b0d3e33b64a44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f163406c72d8e2cc9abd",
+    "url": "/static/css/main.d9a09af5.chunk.css"
+  },
+  {
+    "revision": "d6fd84ebc3bfba82e0b2",
+    "url": "/static/js/2.04ab11d5.chunk.js"
+  },
+  {
+    "revision": "f163406c72d8e2cc9abd",
+    "url": "/static/js/main.20e56824.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
